c8248d177f875f67dc1aab67f0dd108f5580d4bc
MPetrov99 <98600216+MPetrov99@users.noreply.github.com>
2025-09-09 16:04:35 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-const isGh = process.env.VITE_TARGET === 'gh' 
+const isGh = process.env.VITE_TARGET === 'gh'
 
 export default defineConfig({
   plugins: [react()],
-  base: isGh ? '/',
+  base: isGh ? '/React_Task_Spa/' : '/',
 })
